feda2db4cf9728f87cbe18f107a35a36203d9eec
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-01-08 15:41:27 -0500

    add config files

--- modatasubmission/app.py
+++ modatasubmission/app.py
@@ -1,3 +1,16 @@
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
 import sys
 
 import flask
@@ -5,21 +18,31 @@ from flask import Flask
 from mohawk import Receiver
 from werkzeug.wrappers import Response
 
+from modatasubmission.storage import Storage
 from pyLibrary import convert, jsons, strings
 from pyLibrary.debugs import constants
 from pyLibrary.debugs import startup
 from pyLibrary.debugs.exceptions import Except
 from pyLibrary.debugs.logs import Log
+from pyLibrary.maths.randoms import Random
+from pyLibrary.times.dates import Date
+from pyLibrary.times.durations import HOUR
+
+RESPONSE_CONTENT_TYPE = b"application/json"
 
 
 all_creds = []
+containers = {}
+config=None
+
 app = Flask(__name__)
+
 
 @app.route('/', defaults={'path': ''}, methods=['GET'])
 @app.route('/<path:path>')
 def service(path):
     try:
-        request=flask.request
+        request = flask.request
         auth = request.headers['Authorization']
         user = strings.between(auth, 'id="', '",')
         try:
@@ -29,17 +52,34 @@ def service(path):
                 request.url,
                 request.method,
                 content=request.data,
-                content_type=request.headers['Content-Type']
+                content_type=request.headers['Content-Type'],
+                nonce=seen_nonce
             )
         except Exception, e:
             e = Except.wrap(e)
-            Log.error("Authentication failed", cause=e)
+            raise Log.error("Authentication failed", cause=e)
+
 
         permissions = lookup_user(user)
         if path not in user.resources:
             Log.error("{{user}} not allowed access to {{resource}}", user=permissions.hawk.id, resource=path)
 
-        submit_data(path, permissions, request.data)
+        link = submit_data(path, permissions, request.data)
+
+        response_content = convert.unicode2utf8(convert.value2json({"link": link}))
+        receiver.respond(
+            content=response_content,
+            content_type=RESPONSE_CONTENT_TYPE
+        )
+
+        return Response(
+            response_content,
+            status=200,
+            headers={
+                'Server-Authorization': receiver.response_header,
+                "Content-type": RESPONSE_CONTENT_TYPE
+            }
+        )
 
     except Exception, e:
         e = Except.wrap(e)
@@ -48,17 +88,31 @@ def service(path):
             convert.unicode2utf8(convert.value2json(e)),
             status=400,
             headers={
-                "access-control-allow-origin": "*",
                 "Content-type": "application/json"
             }
         )
 
 
-def submit_data(path, permissions, data):
-    # CONFIRM THIS IS JSON
-    confirmed_data = convert.value2json(convert.json2value(data))
+def submit_data(bucket, permissions, body):
+    global containers
+    # CONFIRM THIS IS JSON, AND ANNOTATE
+    data = {
+        "etl": {
+            "user": permissions.hawk.id,
+            "bucket": bucket,
+            "timestamp": Date.now()
+        },
+        "data": convert.json2value(body)
+    }
+    confirmed_data = convert.value2json(data)
 
-    #
+    storage = containers.get(bucket)
+    if storage==None:
+        storage = containers[bucket] = Storage(bucket=bucket, public=True, settings=config.aws)
+    link = storage.add(confirmed_data)
+    return link
+
+
 
 
 def lookup_user(sender):
@@ -72,15 +126,37 @@ def lookup_credentials(sender):
     return lookup_user(sender).hawk
 
 
+seen = {}
+
+def seen_nonce(sender_id, nonce, timestamp):
+    global seen
+    key = '{id}:{nonce}:{ts}'.format(
+        id=sender_id,
+        nonce=nonce,
+        ts=timestamp
+    )
+
+    if Random.int(1000) == 0:
+        old = (Date.now()-HOUR).unix
+        seen = {k: v for k, v in seen.items() if v["timestamp"] >= old}
+
+    if seen.get(key):
+        return True
+    else:
+        seen[key] = {"timestamp": timestamp}
+        return False
+
+
 def main():
     global all_creds
+    global config
 
     try:
         config = startup.read_settings()
         constants.set(config.constants)
         Log.start(config.debug)
 
-        all_creds = jsons.ref.get("file://resources/config/server/example.json")
+        all_creds = jsons.ref.get("file://resources/config/server/server.json")
 
         app.run(**config.flask)
     except Exception, e:
@@ -89,7 +165,6 @@ def main():
         Log.stop()
 
     sys.exit(0)
-
 
 
 if __name__ == '__main__':
