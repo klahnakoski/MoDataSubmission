d2f4acf0c87153868d55a333a12b3486402eb542
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-01-09 10:00:23 -0500

    fix script

--- pyLibrary/thread/multiprocess.py
+++ pyLibrary/thread/multiprocess.py
@@ -34,7 +34,7 @@ class Process(object):
                 params,
                 stdin=subprocess.PIPE,
                 stdout=subprocess.PIPE,
-                # stderr=subprocess.PIPE,
+                stderr=subprocess.PIPE,
                 bufsize=-1,
                 cwd=cwd,
                 env=env
--- resources/scripts/run.sh
+++ resources/scripts/run.sh
@@ -3,5 +3,6 @@ sudo -i
 export PYTHONPATH=.
 export HOME=/home/ec2-user
 cd ~/MoDataSubmission
-nohup python27 modatasubmission/app.py --settings=resources/config/prod.json
+nohup python27 modatasubmission/app.py --settings=resources/config/prod.json &
+
 disown -h
